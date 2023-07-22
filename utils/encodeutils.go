f8e36ccebcb65040385c9e316adf21040e19651b
atanda0x <atandanafiu21@gmail.com>
2023-07-21 20:19:29 -0700

    encoded file

--- utils/encodeutils.go
+++ utils/encodeutils.go
@@ -1,27 +1,34 @@
-package models
+package base62
 
 import (
-	"database/sql"
-	"log"
+	"math"
+	"strings"
 )
 
-func InitDB() (*sql.DB, error) {
-	var err error
-	db, err := sql.Open("postgres", "postgres:atada0x:ethereumsolana@localhost/mydb?sslmode=disable")
-	if err != nil {
-		return nil, err
-	} else {
-		stmt, err := db.Prepare("CREATE TABLE IF NOT EXIST WEB_URL(ID SERIAL PRIMARY KEY, URL TEXT NOT NULL);")
-		if err != nil {
-			log.Println(err)
-			return nil, err
-		}
-		res, err := stmt.Exec()
-		log.Println(res)
-		if err != nil {
-			log.Println(err)
-			return nil, err
-		}
-		return db, nil
+const base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const b = 62
+
+// Func encode the given db ID to a base62 string
+func ToBase62(num int) string {
+	r := num % b
+	res := string(base[r])
+	div := num / b
+	q := int(math.Floor(float64(div)))
+
+	for q != 0 {
+		r = q % b
+		temp := q / b
+		q = int(math.Floor(float64(temp)))
+		res = string(base[int(r)]) + res
 	}
+	return string(res)
+}
+
+// Func that decode a given base62 strint to db ID
+func ToBase10(str string) int {
+	res := 0
+	for _, r := range str {
+		res = (b * res) + strings.Index(base, string(r))
+	}
+	return res
 }
