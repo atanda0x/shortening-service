1d499f24aa03943ff5f2615506751a5bb8d6c3c9
atanda0x <atandanafiu21@gmail.com>
2023-07-21 21:34:43 -0700

    everything working

--- models/models.go
+++ models/models.go
@@ -3,15 +3,18 @@ package models
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func InitDB() (*sql.DB, error) {
 	var err error
-	db, err := sql.Open("postgres", "postgres:atada0x:ethereumsolana@localhost/mydb?sslmode=disable")
+	db, err := sql.Open("postgres", "postgres://atanda0x:ethereumso1ana@localhost/mydb?sslmode=disable")
 	if err != nil {
 		return nil, err
 	} else {
-		stmt, err := db.Prepare("CREATE TABLE IF NOT EXIST WEB_URL(ID SERIAL PRIMARY KEY, URL TEXT NOT NULL);")
+		// Create model for our URL service
+		stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS WEB_URL(ID SERIAL PRIMARY KEY, URL TEXT NOT NULL);")
 		if err != nil {
 			log.Println(err)
 			return nil, err
